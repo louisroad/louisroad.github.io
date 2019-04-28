9da50fbfab091f8cd1458e01bfd6d752d96064e3
SalJose24 <saljose24@gmail.com>
2019-04-27 23:44:30 -0700

    Changed app.js

---
+++ precache-manifest.3b692807761d576f79138891a25ff258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53ea9634e2f87a7c23866f06a400b17",
+    "url": "/louisroad/index.html"
+  },
+  {
+    "revision": "687e539d5a6e2cb1007a",
+    "url": "/louisroad/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/louisroad/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "687e539d5a6e2cb1007a",
+    "url": "/louisroad/static/js/main.b58ca7e2.chunk.js"
+  },
+  {
+    "revision": "939a919e4f1ad84f335c",
+    "url": "/louisroad/static/js/runtime~main.759c0131.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/louisroad/static/media/logo.5d5d9eef.svg"
+  }
+]);
